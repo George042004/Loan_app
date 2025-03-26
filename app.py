dab70ecd0737b41bff04877e5b36517e652a8cbf
George <georgemullarm045@gmail.com>
2025-03-26 19:19:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ married=st.number_input('Enter married status Married:1 Unmarried:0')
 income=st.number_input('Enter your income in thousands')
 la=st.number_input('Enter loan amount')
 if st.button('predict approval'):
-    prediction=model.predict([[gender,married,income,la]])
+    prediction=model.predict([gender,married,income,la])
     if prediction=='Y':
         st.text('Loan approved')
     else:
